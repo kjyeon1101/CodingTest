868c147465402fe2b6f30c9bd8c5f2fa012cfb07
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-06-28 13:12:57 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,10 @@
+import math
+
+def solution(left, right):
+    answer = 0
+    for n in range(left, right+1):
+        if math.sqrt(n) == int(math.sqrt(n)):
+            answer -= n
+        else:
+            answer += n
+    return answer
