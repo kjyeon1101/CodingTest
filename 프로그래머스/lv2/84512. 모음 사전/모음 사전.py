aef328802a112ff801eaba29dba4b6e43781bd54
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-07-10 16:58:58 +0900

    [level 2] Title: 모음 사전, Time: 0.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -6,8 +6,6 @@ def solution(word):
     for i in range(1, 6):
         for a in product(alphabet, repeat=i):
             dictionary.append("".join(a))
-            if "".join(a) == word:
-                break
     dictionary = sorted(dictionary)
     answer = dictionary.index(word)+1
     return answer
