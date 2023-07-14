e1031f1669f29b0f7cd4c7bb8121896b7b7f27d4
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-07-15 03:00:04 +0900

    [level 4] Title: 도둑질, Time: 611.95 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/lv4/42897. 도둑질/도둑질.py
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.py
@@ -1,14 +1,14 @@
 def solution(money):
-    l = len(money)
     s1 = [m for m in money]
     s2 = [m for m in money]
-    s1[1] = -1
-    s2[0] = -1
-    s1[2] = s1[0] + s1[2]
     
-    for i in range(3, l):
-        s1[i] += max(s1[i-2], s1[i-3])
-        s2[i] += max(s2[i-2], s2[i-3])
-
-    answer = max(max(s1[l-2], s1[l-3]), max(s2[l-1], s2[l-2]))
+    s1[1] = s1[0]
+    for i in range(2, len(money)-1):
+        s1[i] = max(s1[i-1], s1[i-2]+s1[i])
+    
+    s2[0] = 0
+    for i in range(2, len(money)):
+        s2[i] = max(s2[i-1], s2[i-2]+s2[i])
+    
+    answer = max(s1[-2], s2[-1])
     return answer
