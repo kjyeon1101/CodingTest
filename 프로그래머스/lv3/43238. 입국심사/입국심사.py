4e74068cc039b6b0c592edda48a9b5c8e9cd7422
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-07-22 00:03:06 +0900

    [level 3] Title: 입국심사, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -0,0 +1,17 @@
+def solution(n, times):
+    answer = 0
+    start = 1
+    end = n * times[len(times)-1]
+    
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for t in times:
+            cnt += (mid // t)
+        if cnt >= n:
+            answer = mid
+            end = mid - 1
+        elif cnt < n:
+            start = mid + 1
+
+    return answer
