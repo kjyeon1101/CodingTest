04e3432b72b475b13c6656cb1a4baff4b30496e3
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-07-25 17:46:41 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 344 ms, Memory: 52820 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dots = []
+for _ in range(N):
+  x, y = map(int, input().split())
+  dots.append((x,y))
+
+dots = sorted(dots, key=lambda x:(x[1], x[0]))
+
+for x, y in dots:
+  print(x, y)
