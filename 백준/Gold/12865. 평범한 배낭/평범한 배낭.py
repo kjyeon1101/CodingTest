c00d0004054fd154f3e5d822d541a44d5f7e7873
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-08-13 20:59:37 +0900

    [Gold V] Title: 평범한 배낭, Time: 4848 ms, Memory: 282468 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,20 +1,17 @@
-import sys
-input = sys.stdin.readline
-
 N, K = map(int, input().split())
-things = []
+things = [(0,0)]
 P = [[0 for _ in range(K+1)] for _ in range(N+1)]
 for _ in range(N):
   W, V = map(int, input().split())
   things.append((W,V))
 
-for i, row in enumerate(P[1:]):
+for i in range(1, N+1):
   w = things[i][0]
   v = things[i][1]
-  for j, r in enumerate(row[1:]):
-    if w > j+1:
-      P[i+1][j+1] = P[i][j+1]
+  for j in range(1, K+1):
+    if w > j:
+      P[i][j] = P[i-1][j]
     else:
-      P[i+1][j+1] = max(P[i][j+1-w] + v, P[i][j+1])
+      P[i][j] = max(P[i-1][j-w] + v, P[i-1][j])
 
 print(P[-1][-1])
