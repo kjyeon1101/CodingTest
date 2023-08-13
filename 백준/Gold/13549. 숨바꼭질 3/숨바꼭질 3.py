b628b3fe52c1550d4a8582a7d04962c6d3f7e030
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-08-14 01:22:57 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 35400 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,19 +1,20 @@
-import sys
-input = sys.stdin.readline
-
 from collections import deque
 
 N, K = map(int, input().split())
-queue = deque([(N, 0)])
-visited = [False] * 100001
-visited[N] = True
+dq = deque([N])
+time = [-1] * 100001
+time[N] = 0
 
-while queue:
-  now, time = queue.popleft()
+while dq:
+  now = dq.popleft()
   if now == K:
-    print(time)
+    print(time[now])
     break
-  for next, second in [(now*2, time), (now-1, time+1), (now+1, time+1)]:
-    if 0 <= next <= 100000 and visited[next] == False:
-      queue.append((next, second))
-      visited[next] = True
+  for next in [now-1, now+1, 2*now]:
+    if 0 <= next <= 100000 and time[next] == -1:
+      if next == 2*now:
+        time[next] = time[now]
+        dq.appendleft(next)
+      else:
+        time[next] = time[now] + 1
+        dq.append(next)
