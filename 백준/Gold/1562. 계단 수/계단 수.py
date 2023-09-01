eb54be82f7e54d4efb479067bdda4e22c2ab468b
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-09-01 17:16:18 +0900

    [Gold I] Title: 계단 수, Time: 688 ms, Memory: 39188 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [[[0 for _ in range(1<<10)] for _ in range(10)] for _ in range(N+1)]
+
+for i in range(1, 10):
+  dp[1][i][1<<i] = 1
+
+for i in range(2, N+1):
+  for j in range(10):
+    for k in range(1<<10):
+      if j == 0:
+        dp[i][j][k|(1<<j)] += dp[i-1][j+1][k]
+      elif j == 9:
+        dp[i][j][k|(1<<j)] += dp[i-1][j-1][k]
+      else:
+        dp[i][j][k|1<<j] += dp[i-1][j-1][k] + dp[i-1][j+1][k]
+      dp[i][j][k|(1<<j)] %= 1000000000
+
+sum = 0
+for i in range(10):
+  sum += dp[N][i][(1<<10)-1]%1000000000
+
+print(sum%1000000000)
