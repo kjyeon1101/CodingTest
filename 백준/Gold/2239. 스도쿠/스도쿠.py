3b3962fdb60546e15f2add7bac8c2c082d2c7abe
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-09-03 14:28:08 +0900

    [Gold IV] Title: 스도쿠, Time: 5020 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,39 +1,46 @@
 import sys
 input = sys.stdin.readline
 
-def check(i,j,p):
-  if p in board[i]:
-    return False
-  elif p in [board[k][j] for k in range(9)]:
-    return False
-  else:
-    x = (i//3) * 3
-    y = (j//3) * 3
-    for a in range(3):
-      for b in range(3):
-        if p == board[x+a][y+b]:
-          return False
-  return True
+def possible_numbers(i,j):
+  return [l for l in range(1,10) if row_check[i][l] and col_check[j][l] and box_check[(i//3)*3+j//3][l]]
 
 def backtracking(k):
   if k >= len(zero):
     for row in board:
-      print("".join([str(b) for b in row]))
+      print("".join(map(str, row)))
     exit()
     
   i = zero[k][0]
   j = zero[k][1]
 
-  for p in range(1,10):
-    if check(i,j,p):
-      board[i][j] = p
-      backtracking(k+1)
-      board[i][j] = 0
-
+  for p in possible_numbers(i,j):
+    board[i][j] = p
+    row_check[i][p] = False
+    col_check[j][p] = False
+    box_check[(i//3)*3+j//3][p] = False
+    backtracking(k+1)
+    board[i][j] = 0
+    row_check[i][p] = True
+    col_check[j][p] = True
+    box_check[(i//3)*3+j//3][p] = True
 
 board = []
 for _ in range(9):
   row = input().strip()
   board.append([int(r) for r in row])
-zero = [(i,j) for i in range(9) for j in range(9) if board[i][j] == 0]
+  
+zero = []
+row_check = [[True]*10 for _ in range(9)]
+col_check = [[True]*10 for _ in range(9)]
+box_check = [[True]*10 for _ in range(9)]
+
+for i in range(9):
+  for j in range(9):
+    if board[i][j] == 0:
+      zero.append((i,j))
+    else:
+      row_check[i][board[i][j]] = False
+      col_check[j][board[i][j]] = False
+      box_check[(i//3)*3+j//3][board[i][j]] = False
+      
 backtracking(0)
