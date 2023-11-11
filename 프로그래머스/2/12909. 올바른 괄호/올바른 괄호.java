e27820d75741331ce65273cb797c2a5491c3603f
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2023-11-11 14:41:36 +0900

    [level 2] Title: 올바른 괄호, Time: 20.15 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i=0;i<s.length();i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.empty()) {
+                    answer = false;
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        
+        if (!stack.empty()) {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
