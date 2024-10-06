f008d2f05a63b0cf2108443ba020b6e17f173b45
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2024-10-07 00:28:06 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,25 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int idx = 0;
-        for(int c=0;c<commands.length;c++){
-            int i = commands[c][0];
-            int j = commands[c][1];
-            int k = commands[c][2];
-            List<Integer> subArray = new ArrayList<>();
-            for(int a=i-1;a<j;a++) {
-                subArray.add(array[a]);
+        int i=0;
+        int j=0;
+        int k=0;
+        List<Integer> arrayList = new ArrayList<>();
+        for(int idx=0;idx<array.length;idx++) {
+            arrayList.add(array[idx]);
+        }
+        
+        for(int idx=0;idx<commands.length;idx++) {
+            i = commands[idx][0];
+            j = commands[idx][1];
+            k = commands[idx][2];
+            List<Integer> subList = new ArrayList<>();
+            for (Integer n : arrayList.subList(i-1,j)) {
+                subList.add(n);
             }
-            Collections.sort(subArray);
-            answer[idx++] = subArray.get(k-1);
+            Collections.sort(subList);
+            answer[idx] = subList.get(k-1);
         }
         return answer;
     }
