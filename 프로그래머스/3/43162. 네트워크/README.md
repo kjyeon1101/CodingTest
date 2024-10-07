eb3b7359d5cfc15dd17cde800b70e41643a86ec8
Jaeyeon Kim <63956478+kjyeon1101@users.noreply.github.com>
2024-10-07 21:15:27 +0900

    [level 3] Title: 네트워크, Time: 2.76 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 83.6 MB, 시간: 0.14 ms
+메모리: 70.6 MB, 시간: 2.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 15:58:2
+2024년 10월 07일 21:15:23
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,38 @@
+import java.util.*;
 class Solution {
+    ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
     boolean[] visited;
-    
-    void dfs(int[][] graph, int v) {
-        visited[v] = true;
-        for(int i=0;i<visited.length;i++) {
-            if (graph[v][i] == 1 && visited[i] == false) {
-                dfs(graph, i);
-            }
-        }
-    }
-    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[n];
-        
+        for(int i=0;i<n;i++) {
+            graph.add(new ArrayList<>());
+            visited[i] = false;
+        }
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if (computers[i][j] == 1) {
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }
+            }
+        }
+
         for(int i=0;i<n;i++) {
             if (visited[i] == false) {
-                dfs(computers, i);
+                dfs(i);
                 answer += 1;
             }
         }
-        
         return answer;
+    }
+    
+    public void dfs(int v) {
+        visited[v] = true;
+        for(int next : graph.get(v)) {
+            if (visited[next] == false) {
+                dfs(next);
+            }
+        }
     }
 }
